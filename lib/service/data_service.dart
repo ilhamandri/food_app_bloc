8ba063b801cb565afb99339741faa8236424b894
ilhamandri <ilhamandriandri@gmail.com>
2021-06-07 14:08:05 +0700

    try to fetch banner image

--- lib/service/data_service.dart
+++ lib/service/data_service.dart
@@ -1,5 +1,15 @@
+import 'package:food_app_bloc/models/banner.dart';
 import 'package:http/http.dart' as http;
 
 class DataService {
   String _baseUrl = 'https://private-3f7641-myfood2.apiary-mock.com/';
+
+  Future<List<Banner>> fetchBannerImage() async {
+    Map<String, String> headers;
+    headers['Content-Type'] = 'application/json';
+
+    var url = Uri.parse(_baseUrl + 'banner');
+    var response = await http.get(url, headers: headers);
+    print(response.body);
+  }
 }
