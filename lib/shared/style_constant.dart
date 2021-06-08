ac2c5000d4dfbe97bc23542d23811c73ce2b65d1
ilhamandri <ilhamandriandri@gmail.com>
2021-06-08 11:19:30 +0700

    add textstyle constant

---
+++ lib/shared/style_constant.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class StyleConstant {
+  static TextStyle normal16 = TextStyle(fontWeight: FontWeight.w400, fontSize: 16);
+
+  static TextStyle light16 = TextStyle(fontWeight: FontWeight.w300, fontSize: 16);
+
+  static TextStyle bold16 = TextStyle(fontWeight: FontWeight.w700, fontSize: 16);
+
+  static TextStyle bold12 = TextStyle(fontWeight: FontWeight.w700, fontSize: 12);
+
+  static TextStyle bold14 = TextStyle(fontWeight: FontWeight.w700, fontSize: 14);
+}
