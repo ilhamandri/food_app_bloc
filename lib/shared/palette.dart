f15fbd2e86f3401a7c854ea77227d0d8ccd58dd2
ilhamandri <ilhamandriandri@gmail.com>
2021-06-08 16:51:54 +0700

    create detail screen

--- lib/shared/palette.dart
+++ lib/shared/palette.dart
@@ -4,4 +4,6 @@ class Palette {
   static Color green = Color(0xff27AE60);
   static Color gradient1 = Color(0xffcc2b5e);
   static Color gradient2 = Color(0xff753a88);
+  static Color divider = Color(0xffe5e5e5);
+  static Color border = Color(0xffe0e0e0);
 }
--- lib/views/detail/detail_screen.dart
+++ lib/views/detail/detail_screen.dart
@@ -1,6 +1,8 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:food_app_bloc/models/product.dart';
+import 'package:food_app_bloc/extensions/string_extension.dart';
+import 'package:food_app_bloc/shared/palette.dart';
 
 class DetailScreen extends StatelessWidget {
   @override
@@ -13,29 +15,52 @@ class DetailScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          CachedNetworkImage(
-            imageUrl: args.image,
-            width: MediaQuery.of(context).size.width,
-            height: 160,
-            fit: BoxFit.cover,
-          ),
+          _buildItemImage(args),
           Expanded(
             child: Padding(
               padding: const EdgeInsets.all(20),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text('Ayam Bakar Utuh Khas Dapoer Mama'),
-                      Text('Rp 20.000'),
+                      Text(args.title.capitalizeFirstLetter()),
+                      Text('Rp ' + args.price.toString()),
                     ],
                   ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Text(args.description),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Row(
+                    children: [],
+                  ),
                   Spacer(),
-                  Divider(),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text('Add to Basket'),
+                  Divider(
+                    height: 2,
+                    color: Palette.divider,
+                  ),
+                  Container(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        primary: Palette.green,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 15),
+                        child: Text('Add to Basket'),
+                      ),
+                    ),
                   )
                 ],
               ),
@@ -43,6 +68,15 @@ class DetailScreen extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+
+  Widget _buildItemImage(Product product) {
+    return CachedNetworkImage(
+      imageUrl: product.image,
+      width: double.infinity,
+      height: 160,
+      fit: BoxFit.cover,
     );
   }
 }
