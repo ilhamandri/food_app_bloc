a0d76a9b213487f3baf02c1f207ac00c6727544b
ilhamandri <ilhamandriandri@gmail.com>
2021-06-08 11:06:04 +0700

    styling catalog & add color palette

---
+++ lib/shared/palette.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+class Palette {
+  static Color green = Color(0xff27AE60);
+  static Color gradient1 = Color(0xffcc2b5e);
+  static Color gradient2 = Color(0xff753a88);
+}
--- lib/shared/widgets/catalog_item.dart
+++ lib/shared/widgets/catalog_item.dart
@@ -1,6 +1,8 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:food_app_bloc/models/product.dart';
+import 'package:food_app_bloc/extensions/string_extension.dart';
+import 'package:food_app_bloc/shared/palette.dart';
 
 class CatalogItem extends StatelessWidget {
   final Product product;
@@ -11,27 +13,48 @@ class CatalogItem extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        ClipRRect(
-          borderRadius: BorderRadius.all(Radius.circular(10)),
-          child: CachedNetworkImage(
-            imageUrl: product.image,
-            fit: BoxFit.cover,
-            errorWidget: (context, url, error) => Icon(Icons.error),
-            width: double.infinity,
-            height: 167,
-          ),
+        Stack(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              child: CachedNetworkImage(
+                imageUrl: product.image,
+                fit: BoxFit.cover,
+                errorWidget: (context, url, error) => Icon(Icons.error),
+                width: double.infinity,
+                height: 167,
+              ),
+            ),
+            _buildPriceGradient(),
+          ],
         ),
-        SizedBox(
-          height: 10,
-        ),
-        Text(product.title),
         SizedBox(
           height: 10,
         ),
         Text(
-          product.price.toString(),
+          product.title.capitalizeFirstLetter(),
+          overflow: TextOverflow.clip,
         ),
       ],
+    );
+  }
+
+  Widget _buildPriceGradient() {
+    return Positioned(
+      bottom: 0,
+      child: Container(
+        // width: 100,
+        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(15), bottomRight: Radius.circular(15)),
+          gradient: LinearGradient(colors: [Palette.gradient1, Palette.gradient2]),
+        ),
+        child: Text(
+          'Rp ' + product.price.toString(),
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
     );
   }
 }
